f7b0c2e37c4cdac69dbfd104481f9023f4a4cc2c
kasuboski <kasuboski@wisc.edu>
2014-01-03 16:54:57 -0600

    Initial Commit

---
+++ Sheepshead iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sheepshead iOS
+//
+//  Created by Josh Kasuboski on 1/3/14.
+//  Copyright (c) 2014 Josh Kasuboski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sheepshead iOSTests/Sheepshead_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Sheepshead_iOSTests.m
+//  Sheepshead iOSTests
+//
+//  Created by Josh Kasuboski on 1/3/14.
+//  Copyright (c) 2014 Josh Kasuboski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sheepshead_iOSTests : XCTestCase
+
+@end
+
+@implementation Sheepshead_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
